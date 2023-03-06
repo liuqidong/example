09ec3aa974eea33cd5af1fc87a03904a3df25510
liuqidong <liuqidong_hn@163.com>
2023-03-06 23:40:30 +0800

    加入并发代码

---
+++ jucExample/src/main/java/com/liuqidong/jucexample/JucExampleApplication.java
@@ -0,0 +1,13 @@
+package com.liuqidong.jucexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JucExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JucExampleApplication.class, args);
+    }
+
+}
---
+++ jucExample/src/test/java/com/liuqidong/jucexample/JucExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liuqidong.jucexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JucExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
